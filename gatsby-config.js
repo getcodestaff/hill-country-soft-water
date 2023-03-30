08f9a00247399adfce044e7c92799c2a5b314280
witcradg <witcradg@gmail.com>
2023-03-30 11:45:46 -0700

    WIP adding d8gblog WP instance

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,28 @@ module.exports = {
     author: `@witcradg`,
   },
   plugins: [
+    {
+        /**
+         * First up is the WordPress source plugin that connects Gatsby
+         * to your WordPress site.
+         *
+         * visit the plugin docs to learn more
+         * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
+         *
+         */
+        resolve: `gatsby-source-wordpress`,
+        options: {
+          // the only required plugin option for WordPress is the GraphQL url.
+          url:
+            process.env.WPGRAPHQL_URL || `https://d8gblog.wpengine.com/graphql`,
+          auth: {
+            htaccess: {
+              username: "demo",
+              password: "password",
+            },
+          },
+        },
+      },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
