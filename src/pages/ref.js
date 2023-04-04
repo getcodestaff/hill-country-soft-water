82768e22e37844da996bf52f1cb536172ba09ec2
witcradg <witcradg@gmail.com>
2023-04-04 12:26:38 -0700

    WIP Ref improvements

--- src/pages/ref.js
+++ src/pages/ref.js
@@ -34,10 +34,10 @@ const Ref = () => {
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>Items are in order of appearance</h1>
+      <h1 style={styles.title}>Items Need To Be Consistent and Aligned Horizontally</h1>
       <div style={styles.columns}>
         <div style={styles.column}>
-          <h2 style={styles.header}>Card</h2>
+          <h2 style={styles.header}>Card (Service 1-8 order)</h2>
           <div style={styles.item}>water softeners</div>
           <div style={styles.item}>water filtration</div>
           <div style={styles.item}>reverse osmosis</div>
@@ -48,17 +48,6 @@ const Ref = () => {
           <div style={styles.item}>well water systems (word order?)</div>
         </div>
         <div style={styles.column}>
-          <h2 style={styles.header}>Pages*</h2>
-          <div style={styles.item}>water-softeners.js</div>
-          <div style={styles.item}>water-filtration.js</div>
-          <div style={styles.item}>reverse-osmosis.js</div>
-          <div style={styles.item}>**doors**</div>
-          <div style={styles.item}>hard-water-filters.js</div>
-          <div style={styles.item}>house-water-filters.js</div>
-          <div style={styles.item}>water-testing.js</div>
-          <div style={styles.item}>water-treatment.js</div>
-        </div>
-        <div style={styles.column}>
           <h2 style={styles.header}>Footer Links</h2>
           <div style={styles.item}>water-softeners</div>
           <div style={styles.item}>water-filtrations</div>
@@ -67,24 +56,45 @@ const Ref = () => {
           <div style={styles.item}>alkaline-water-filters</div>
           <div style={styles.item}>hard-water-systems</div>
           <div style={styles.item}>whole-house-water-filtrations-systems</div>
-          <div style={styles.item}>well-water-systems</div> 
+          <div style={styles.item}>well-water-systems</div>
         </div>
         <div style={styles.column}>
-          <h2 style={styles.header}>Menu order</h2>
+          <h2 style={styles.header}>Menu Order</h2>
           <div style={styles.item}>WATER SOFTENERS</div>
           <div style={styles.item}>WATER FILTRATION</div>
           <div style={styles.item}>REVERSE OSMOSIS</div>
-          <div style={styles.item}>**doors**</div>
-          <div style={styles.item}>HARD WATER FILTERS</div>
+          <div style={styles.item}>&nbsp;</div>
           <div style={styles.item}>WATER TESTING</div>
+          <div style={styles.item}>HARD WATER FILTERS</div>
           <div style={styles.item}>HOUSE WATER FILTERS</div>
           <div style={styles.item}>WATER WELL SYSTEMS</div>
         </div>
+        <div style={styles.column}>
+          <h2 style={styles.header}>Pages</h2>
+          <div style={styles.item}>water-softeners.js</div>
+          <div style={styles.item}>water-filtration.js</div>
+          <div style={styles.item}>reverse-osmosis.js</div>
+          <div style={styles.item}>&nbsp;</div>
+          <div style={styles.item}>water-testing.js</div>
+          <div style={styles.item}>hard-water-filters.js</div>
+          <div style={styles.item}>house-water-filters.js</div>
+          <div style={styles.item}>water-treatment.js</div>
+        </div>
       </div>
-      <p className="w-[80%]">
-        *Pages: Page order in vscode is alphabetical. Make sure the other
-        columns align and just arrange this to align with them.
-      </p>
+      <div className="w-[80%]">
+        <p>
+          Cards: Cards are listed in the order currently coded and presented on the
+          homepage.
+        </p>
+        <p>
+          Pages: Page order in vscode is alphabetical. Make sure the other
+          columns align and just arrange this to align with them.
+        </p>
+        <p>
+          Services: Service1...Service8 should match the card order for easy
+          visual reference.
+        </p>
+      </div>
     </div>
   )
 }
