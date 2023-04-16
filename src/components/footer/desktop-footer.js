169484c80323269d638933e1be5a6a3b290f28a6
witcradg <witcradg@gmail.com>
2023-04-16 12:54:34 -0700

    WIP desktop and mobile footer

--- src/components/footer/desktop-footer.js
+++ src/components/footer/desktop-footer.js
@@ -11,6 +11,7 @@ import * as styles from "./footer.module.css"
 export const DesktopFooter = ({ data, areas }) => {
   const { layout } = useContext(LayoutContext)
   const location = layout.location
+  const lcState = location?.lcState
 
   const [areasToggle, setAreasToggle] = useState(false)
 
@@ -18,7 +19,7 @@ export const DesktopFooter = ({ data, areas }) => {
     setAreasToggle(!areasToggle)
   }
 
-    let image = getImage(data.logoImage)
+  let image = getImage(data.logoImage)
 
   const colClasses = location?.map
     ? {
@@ -38,56 +39,61 @@ export const DesktopFooter = ({ data, areas }) => {
     <React.Fragment>
       <footer className="ml-8 hidden lg:block m-auto">
         <div style={{ fontSize: "13px" }}>
-        <div
+          <div
             id="all-columns"
-          className="flex justify-around"
-          style={{ width: "100%", fontSize: "13px" }}
-        >
+            className="flex justify-around"
+            style={{ width: "100%", fontSize: "13px" }}
+          >
             <div id="logo-column-1" className={colClasses.col1}>
-            <div>
-              <nav className="container navbar navbar-expand-lg navbar-dark pl-6">
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapsibleNavbar"
-                >
-                  <span className="navbar-toggler-icon" />
-                </button>
-                  <GatsbyImage className="mix-blend-darken" image={image} alt={data.logoAlt} />
-              </nav>
+              <div>
+                <nav className="container navbar navbar-expand-lg navbar-dark pl-6">
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#collapsibleNavbar"
+                  >
+                    <span className="navbar-toggler-icon" />
+                  </button>
+                  <GatsbyImage
+                    className="mix-blend-darken"
+                    image={image}
+                    alt={data.logoAlt}
+                  />
+                </nav>
+              </div>
+              <div className="p-6">{data.logoColumnText}</div>
+              <div>
+                <FooterSocial data={data} />
+              </div>
             </div>
-            <div className="p-6">{data.logoColumnText}</div>
-            <div>
-              <FooterSocial data={data} />
-            </div>
-          </div>
 
             <div id="service-links-column" className={colClasses.col2}>
-            <h4 className={styles.column_title}>{data.column2header}</h4>
-            <ServicesLinks data={data} classline="mt-2" />
-          </div>
+              <h4 className={styles.column_title}>{data.column2header}</h4>
+              <ServicesLinks data={data} classline="mt-2" />
+            </div>
 
             <div id="service-areas-column" className={colClasses.col3}>
-            <h4 className={styles.column_title}>{data.column3header}</h4>
+              <h4 className={styles.column_title}>{data.column3header}</h4>
               <ServiceAreas
                 data={data}
                 location={location}
                 classline="mt-2"
                 showAreasToggle={toggle}
               />
-          </div>
+            </div>
 
             <div id="contacts-column" className={colClasses.col4}>
-            <h4 className={styles.column_title}>{data.column4header}</h4>
-            <FooterContacts data={data} />
+              <h4 className={styles.column_title}>{data.column4header}</h4>
+              <FooterContacts data={data} />
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
 
+      {/* more... */}
       <div id="all-service-areas">
-        {areasToggle === true || location?.pathname?.includes("texas") ? (
+        {areasToggle === true || location?.pathname?.includes(lcState) ? (
           <section id="service-areas" className="bg-white">
             <div className="mx-6 lg:mx-20 mb-4">
               <h4 className="font-bold mb-2 text-center">Service Areas</h4>
--- src/components/footer/mobile-footer.js
+++ src/components/footer/mobile-footer.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useContext} from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import { FooterSocial } from "./footer-social"
@@ -6,8 +6,12 @@ import { FooterAccordion } from "./footer-accordion"
 import { ServicesLinks } from "./services-links"
 import { ServiceAreas } from "./service-areas"
 import { FooterContacts } from "./footer-contacts"
+import { LayoutContext } from "../../contexts/layout-context"
 
 export const MobileFooter = ({ data }) => {
+    const { layout } = useContext(LayoutContext)
+    const location = layout.location
+
     return (
     <div id="mobile-footer" className="block lg:hidden m-auto">
                 <div className="container m-auto">
@@ -17,7 +21,7 @@ export const MobileFooter = ({ data }) => {
                             <h4 className="footer-title">{data.column2header}</h4>
                             <ServicesLinks data={data} />
                             <h4 className="footer-title">{data.column3header}</h4>
-                            <ServiceAreas data={data} />
+                            <ServiceAreas data={data} location={location} />
                             <h4 className="footer-title">{data.column4header}</h4>
                             <FooterContacts data={data} />
                         </FooterAccordion>
