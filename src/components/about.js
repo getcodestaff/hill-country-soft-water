0be8972f2fb3c7c5b4d70c2f99bc37d8ccfcf264
getcodestaff <66915102+getcodestaff@users.noreply.github.com>
2023-04-24 19:17:21 -0700

    about us page images

--- src/components/about.js
+++ src/components/about.js
@@ -2,9 +2,9 @@ import PropTypes from "prop-types"
 import React from "react"
 import { Banner } from "./banner"
 import threeCarruths from "../images/about-us/three-carruths.jpg"
-import Judah from "../images/about-us/Judah.jpg"
-import Steve from "../images/about-us/Steve.jpg"
-import Micah from "../images/about-us/Micah.jpg"
+import Judah from "../images/water-softeners/hard-water-softener.webp"
+import Steve from "../images/water-softeners/water-softener-system.webp"
+import Micah from "../images/commercial-water-softener.webp"
 
 const About = () => (
   <main className="site-main">
@@ -14,7 +14,7 @@ const About = () => (
         <div className="m-auto w-full">
           <img className="" src={threeCarruths} alt="Micha, Steve and Judah" />
           <h5 className="text-center" id="steve-judah-title">
-            Micah, Steve, and Judah
+            Gaines Soft Water Serving San Antonio, Texas
           </h5>
         </div>
         <div className="m-4 lg:flex lg:flex-col lg:justify-center lg:text-lg">
@@ -28,18 +28,18 @@ const About = () => (
         </div>
         <div className="w-full">
           <div>
-            <img className="m-auto" src={Judah} alt="Judah" />
+            <img className="m-auto" src={Judah} alt="Texas Whole House Water Filters" />
           </div>
-          <h5 className="text-center">Judah</h5>
+          <h5 className="text-center">Texas Whole House Water Filters</h5>
         </div>
       </section>
 
       <section className="content-body lg:flex">
         <div className="w-full">
           <div>
-            <img className="m-auto" src={Micah} alt="Micah" />
+            <img className="m-auto" src={Micah} alt="San Antonio Water Company" />
           </div>
-          <h5 className="text-center">Micah</h5>
+          <h5 className="text-center">San Antonio Water Company</h5>
         </div>
         <div className="m-4 lg:flex lg:flex-col lg:justify-center lg:text-lg">
           RO Systems
