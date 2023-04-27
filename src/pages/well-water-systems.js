1fff60910e2e22957c76d5e1e70e99bd55fb872a
getcodestaff <66915102+getcodestaff@users.noreply.github.com>
2023-04-26 18:30:00 -0700

    add meta

--- src/pages/well-water-systems.js
+++ src/pages/well-water-systems.js
@@ -10,7 +10,9 @@ const WellWaterPage = ({ location }) => (
 )
 
 export const Head = ({ location }) => {
-  return <Seo url={location.href} />
+  const title = "Well Water Filtration Systems San Antonio, TX | Since 1989"
+  const description = "Well Water Filtration Systems San Antonio, TX. Serving the local residents of San Antonio Since 1989. Whole House Filtration for Well Water and Well RO system maintenance, repair."
+  return <Seo url={location.href} title={title} description={description} />
 }
 
 export default WellWaterPage
